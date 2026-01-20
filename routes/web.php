ab924a49acc73fae1083d61574227725503e5098
Ngungila <168288981+Ngungila@users.noreply.github.com>
2026-01-20 08:42:04 +0300

    Rename /resources route to /resource-library to avoid server 403 conflicts

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,12 @@ Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/ministries', [PageController::class, 'ministries'])->name('ministries');
 Route::get('/events', [PageController::class, 'events'])->name('events');
 Route::get('/media', [PageController::class, 'media'])->name('media');
-Route::get('/resources', [PageController::class, 'resources'])->name('resources');
+// Redirect old /resources to new /resource-library to avoid server conflicts
+Route::get('/resources', function () {
+    return redirect('/resource-library', 301);
+});
+
+Route::get('/resource-library', [PageController::class, 'resources'])->name('resources');
 Route::get('/get-involved', [PageController::class, 'getInvolved'])->name('get-involved');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 Route::get('/faq', [PageController::class, 'faq'])->name('faq');
